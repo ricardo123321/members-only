5d94aea8ecdba1b7e6fdd965835885990a02e09e
Shingirayi Mandebvu <shingim1@gmail.com>
2020-07-24 22:37:10 +0200

    Use guard clause and indentation

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,14 +1,14 @@
 module PostsHelper
   def check_s(_user, post)
     post.user.email if user_signed_in?
-    end
+  end
 
   def check_e(_user, current_user, post)
     link_to 'Edit', edit_post_path(post) if user_signed_in? && current_user.id == post.user_id
   end
 
   def check_d(_user, current_user, post)
-    if user_signed_in? && current_user.id == post.user_id
+    return unless user_signed_in? && current_user.id == post.user_id
       link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
     end
   end
